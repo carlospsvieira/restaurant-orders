6681bc3bea1910276f89bca67c11a5dc5adc4068
Carlos Vieira <carlepsvieira@gmail.com>
2023-06-18 18:00:22 -0300

    req 02 more tests

--- tests/dish/test_dish.py
+++ tests/dish/test_dish.py
@@ -1,6 +1,42 @@
-from src.models.dish import Dish  # noqa: F401, E261, E501
+from src.models.dish import Dish
+from src.models.ingredient import Ingredient
+import pytest
 
 
-# Req 2
 def test_dish():
-    pass
+    dish = Dish("Sushi_box", 45.99)
+    assert dish.name == "Sushi_box"
+    assert dish.price == 45.99
+    assert dish.recipe == {}
+
+    # eq and hash
+    dish2 = Dish("Sushi_box", 45.99)
+    dish3 = Dish("Churrasco_individual", 39.99)
+    assert dish == dish2
+    assert dish != dish3
+    assert hash(dish) == hash(dish2)
+    assert hash(dish) != hash(dish3)
+
+    # repr
+    assert repr(dish) == "Dish('Sushi_box', R$45.99)"
+
+    # testing add_ingredient_dependency
+    ingredient1 = Ingredient("Calabresa")
+    ingredient2 = Ingredient("Tomate")
+    dish.add_ingredient_dependency(ingredient1, 2)
+    dish.add_ingredient_dependency(ingredient2, 1)
+    assert dish.recipe == {ingredient1: 2, ingredient2: 1}
+
+    # testing get_restrictions
+    restrictions = dish.get_restrictions()
+    assert restrictions == set()
+
+    # testing get_ingredients
+    ingredients = dish.get_ingredients()
+    expected_ingredients = {ingredient1, ingredient2}
+    assert set(ingredients) == expected_ingredients
+
+    with pytest.raises(TypeError):
+        Dish("Sushi_box", "45.99")
+    with pytest.raises(ValueError):
+        Dish("Sushi_box", -45.99)
